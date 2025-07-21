ebbbb7dade36308aacee34c3faefae0955e630d1
Hidenori Miyoshi <miyohide@gmail.com>
2025-07-21 10:18:16 +0900

    create sample app

---
+++ lib/main.rb
@@ -0,0 +1,20 @@
+require 'aws-sdk-s3vectors'
+
+client = Aws::S3Vectors::Client.new(region: 'us-east-1')
+
+resp = client.list_vector_buckets({max_results: 10})
+
+# respに格納されているS3 VectorsバケットにあるIndexを出力する
+resp.vector_buckets.each do |bucket|
+  vector_bucket_name = bucket.vector_bucket_name
+  puts "S3 Vector buckets #{vector_bucket_name}"
+
+  indexes = client.list_indexes({
+    vector_bucket_name: vector_bucket_name,
+    max_results: 10
+  })
+
+  indexes.each do |i|
+    puts "  Index: #{i.index_name}"
+  end
+end
